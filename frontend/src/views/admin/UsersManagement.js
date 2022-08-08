66c044db14384df8e452345335f03fc7dfb547a4
Alex <la.montesdeoca.p@gmail.com>
2022-08-08 10:19:50 -0500

    UPDATE: frontend corregido

--- frontend/src/views/admin/UsersManagement.js
+++ frontend/src/views/admin/UsersManagement.js
@@ -1,56 +1,83 @@
 import {
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	Button,
 } from '@mui/material';
 import React from 'react';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, calories, fat, carbs, protein, options) {
+	return { name, calories, fat, carbs, protein, options };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+	createData(
+		'Roberto',
+		'Martinez',
+		'Perez',
+		'personal de seguridad',
+		<Button variant='contained' color='primary'>
+			Editar
+		</Button>,
+		<Button variant='contaied' color='primary'>
+			Eliminar
+		</Button>,
+	),
+	createData(
+		'Roberto',
+		'Martinez',
+		'Perez',
+		'personal de seguridad',
+		<Button variant='contained' color='primary'>
+			Editar
+		</Button>,
+		<Button variant='contaied' color='primary'>
+			Eliminar
+		</Button>,
+	),
 ];
 
 export const UsersManagement = () => {
-  return (
-    <div>
-      <h2 className='text-center mb-5'>Gestion de usuarios</h2>
-      <TableContainer component={Paper} sx={{ boxShadow: '2px 2px 2px 1px rgba(0, 0, 0, 0.2);' }}>
-        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-          <TableHead>
-            <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align='right'>Calories</TableCell>
-              <TableCell align='right'>Fat&nbsp;(g)</TableCell>
-              <TableCell align='right'>Carbs&nbsp;(g)</TableCell>
-              <TableCell align='right'>Protein&nbsp;(g)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell component='th' scope='row'>
-                  {row.name}
-                </TableCell>
-                <TableCell align='right'>{row.calories}</TableCell>
-                <TableCell align='right'>{row.fat}</TableCell>
-                <TableCell align='right'>{row.carbs}</TableCell>
-                <TableCell align='right'>{row.protein}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
-  );
+	return (
+		<div>
+			<h2 className='text-center mb-5'>Gestión de Usuarios</h2>
+			<TableContainer
+				component={Paper}
+				sx={{ boxShadow: '2px 2px 2px 1px rgba(0, 0, 0, 0.2);' }}
+			>
+				<Table sx={{ minWidth: 650 }} aria-label='simple table'>
+					<TableHead>
+						<TableRow>
+							<TableCell>Nombre</TableCell>
+							<TableCell align='right'>Apellido Paterno</TableCell>
+							<TableCell align='right'>Apellido Materno</TableCell>
+							<TableCell align='right'>Rol</TableCell>
+							<TableCell align='right'>Opciones</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{rows.map(row => (
+							<TableRow
+								key={row.name}
+								sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+							>
+								<TableCell component='th' scope='row'>
+									{row.name}
+								</TableCell>
+								<TableCell align='right'>{row.calories}</TableCell>
+								<TableCell align='right'>{row.fat}</TableCell>
+								<TableCell align='right'>{row.carbs}</TableCell>
+								<TableCell align='right'>{row.protein}</TableCell>
+								<TableCell align='right'>{row.options}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</div>
+	);
 };
